7aed17dd3aa51ee217a0d64a98663049c0628bbd
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2024-11-17 14:24:28 +0300

    рефактор тестов на test.each

--- src/test/activities.test.ts
+++ src/test/activities.test.ts
@@ -1,11 +1,10 @@
-import { it, expect, describe, beforeEach } from 'vitest'
+import { it, expect, describe, beforeEach, test } from 'vitest'
 import { calculateActivityCompletionPercentage, updateActivity } from '../activities'
 import { Activity } from '../types'
 
 let activity: Activity
 
 beforeEach(() => {
-
   activity = {
     id: '1',
     name: 'test',
@@ -14,31 +13,26 @@ beforeEach(() => {
 })
 
 describe('updateActivity', () => {
-
   const updatedFields: Partial<Activity> = {
     id: '2',
     name: 'updated',
     secondsToComplete: 1300
   }
 
-  
   it('updates original activity', () => {
     expect(updateActivity(activity, updatedFields)).toEqual(updatedFields)
   })
-  
+
   it('returns updated activity', () => {
     const updatedActivity = updateActivity(activity, updatedFields)
     expect(updatedActivity).toEqual(updatedFields)
   })
 
-  it('calculates activity completion percentage', () => {
-
-    const percentageA = calculateActivityCompletionPercentage(activity, 0)
-    const percentageB = calculateActivityCompletionPercentage(activity, 1800)
-    const percentageC = calculateActivityCompletionPercentage(activity, 3600)
-
-    expect(percentageA).toBe(0)
-    expect(percentageB).toBe(50)
-    expect(percentageC).toBe(100)
+  test.each([
+    [0, 0],
+    [1800, 50],
+    [3600, 100]
+  ])('calculateActivityCompletionPercentage(%i) => %i', (seconds, percentage) => {
+    expect(calculateActivityCompletionPercentage(activity, seconds)).toBe(percentage)
   })
 })
